ef6786d152e9b4f3b416d986106361989f2a6246
Lucas Rodrigues Da Silva <lucas.rodrigues@acct.global>
2021-02-20 13:54:33 -0300

    Start - Taks Sass, Html and Watch

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,29 @@
-var gulp = require('gulp');
+const gulp = require('gulp');
+const html = require('gulp-htmlmin');
+const sass = require('gulp-sass');
 
-gulp.task('default', async () => {
-  console.log("HTTP Server Started");
+//sass
+gulp.task('sass', async () => {
+  await gulp.src('./src/assets/scss/*.scss')
+    .pipe(sass({ outputStyle: "compressed" }))
+    .pipe(gulp.dest('./dist/css'));
+})
+
+// html
+gulp.task('html', async () => {
+    await gulp.src('./src/html/*.html')
+      .pipe(html({ collapseWhitespace: true }))
+      .pipe(gulp.dest('./dist/html'));
+})
+
+// watch
+gulp.task('watch', async () => {
+  await gulp.watch('./src/assets/scss/*.scss', gulp.series('sass'));
+  await gulp.watch('./src/html/*.html', gulp.series('html'));
 });
+
+// Default Task
+gulp.task('default', gulp.parallel('sass', 'html', 'watch'));
+
+// exports.default = series(html, sass);
+
